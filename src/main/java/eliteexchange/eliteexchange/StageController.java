781de94684ffa2a4a60b3b01d40158dffd32c1ab
ahmedwaelshenif <ahmadswael@gmail.com>
2024-05-03 17:00:50 +0300

    market update

--- src/main/java/eliteexchange/eliteexchange/StageController.java
+++ src/main/java/eliteexchange/eliteexchange/StageController.java
@@ -1,7 +1,9 @@
 package eliteexchange.eliteexchange;
 import ApplicationElite.Account;
-import ApplicationElite.Securities;
+import ApplicationElite.DataShow;
+import ApplicationElite.Stock;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,43 +11,72 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.Labeled;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
-import java.util.EventObject;
 import java.util.ResourceBundle;
 import java.util.function.UnaryOperator;
-
-import org.controlsfx.control.action.Action;
-
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class StageController implements Initializable {
 
     Account account =new Account();
-    Securities stock=new Securities();
+      
+
+    Stock stock=new Stock();
     private Stage stage;
     private Scene scene;
     private Parent root;
     @FXML
-    private Button delete;
-    @FXML
-    private Button exit;
-    @FXML
-    private ComboBox<String> stocklist;
+    private TableView<DataShow> MarketList;
 
     @FXML
-    private Button add;
+    private TableColumn<DataShow, Float> changePrice;
+
+    @FXML
+    private TableColumn<DataShow, String> company;
+
+    @FXML
+    private TableColumn<DataShow, Float> currentPrice;
+
+    @FXML
+    private TableColumn<DataShow, Float> startPrice;
+    @FXML
+    private TableColumn<DataShow, Float> numberofStocks;
+    @FXML
+    private TextField startprice;
+
+
+
+    @FXML
+    private Button delete;
+    @FXML
+    private Button market;
+    @FXML
+    private Button exit;
+ @FXML
+    private TableView<DataShow> Addtable;
+
+
+    @FXML
+    private Button deleteStock;
+
+    @FXML
+    private TextField stockValue;
+
+
+
+    @FXML
+    private Button addStock;
 
     @FXML
     private TextField companyName;
@@ -61,9 +92,6 @@ public class StageController implements Initializable {
 
     @FXML
     private Button Sellstock;
-
-    @FXML
-    private Button addStock;
 
     @FXML
     Label messagelabel;
@@ -172,33 +200,22 @@ public class StageController implements Initializable {
     }
     @FXML
     void AddstockScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Addscene.fxml"));
-
+  
+        Parent root = FXMLLoader.load(getClass().getResource("AddScene.fxml")); 
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
+        stock.RestoreData();
         stage.show();
+       
     }
     @FXML
-    void confirmAdd(ActionEvent event) {
-        UnaryOperator<TextFormatter.Change> filter = change -> {
-            String text = change.getText();
-            if (text.matches("[0-9]*")) {
-                stock.RestoreData2();
-                return change;
-            }
-            stock.RestoreData2();
-            return null;
-
-
-        };
-        TextFormatter<String> formatter = new TextFormatter<>(filter);
-        numberOfStocks.setTextFormatter(formatter);
-        stock.AddStock(companyName.getText(),Integer.parseInt(numberOfStocks.getText()));
-
-
-
+    void addStock(ActionEvent event) {/////////////////////
+        stock.addStock(companyName.getText(),Integer.parseInt(numberOfStocks.getText()),Float.parseFloat(startprice.getText()));
+        stock.RestoreData();
+Addtable.setItems(stock.returnList());
     }
+
     @FXML
     void BackPressed(ActionEvent event) throws IOException {
         if(account.userOrAdmin().equals("user"))
@@ -234,17 +251,17 @@ public class StageController implements Initializable {
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        if (stocklist != null) {
-            for(int i=2;i<stock.stockList1.length;i++)
-                stocklist.getItems().add(stock.stockList1[i]);
-        }
+       if(company!=null)
+        company.setCellValueFactory(new PropertyValueFactory<>("company"));
+        if(startPrice!=null)
+        startPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        if(numberofStocks!=null)
+        numberofStocks.setCellValueFactory(new PropertyValueFactory<>("number"));
+        if(Addtable!=null)
+        Addtable.setItems(stock.returnList());
+
     }
-    @FXML
-    void deleteButton(ActionEvent event) {
-        System.out.println(stocklist.getValue());
-        stock.DeleteStock(stocklist.getValue());
-        stock.RestoreData2();
-    }
+   
     @FXML
     private void exit(MouseEvent event) throws IOException {
         account.adminSwitch();
@@ -271,6 +288,34 @@ public class StageController implements Initializable {
         account.adminSwitch();
         System.exit(0);
     }
+  
+
+ 
+    @FXML
+    void deleteStock(ActionEvent event) {
+       // int selectedID = Addtable.getSelectionModel().getSelectedIndex();
+       // Addtable.getItems().remove(selectedID);
+        DataShow selectedStock = Addtable.getSelectionModel().getSelectedItem();
+        if (selectedStock != null) {
+            String selectedName = selectedStock.getCompany();
+            System.out.println("Selected stock name: " + selectedName);
+            stock.DeleteStock(selectedName);
+             int selectedID = Addtable.getSelectionModel().getSelectedIndex();
+        Addtable.getItems().remove(selectedID);
+        }
+    }
+    @FXML
+    void MarketPressed(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("Market.fxml"));
+            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+stock.RestoreData();
+    }
+
+  
+
 
 }
 
