4530c0c923499b09eff987debf25ee6113b7b7b0
ahmedwaelshenif <ahmadswael@gmail.com>
2024-05-06 22:34:50 +0300

    random prices update

---
+++ src/main/java/ApplicationElite/PriceUpdater.java
@@ -0,0 +1,23 @@
+package ApplicationElite;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+    public class PriceUpdater {
+        public static void main(String[] args) {
+            Stock stocks=new Stock();
+            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+            // Schedule the task to run every 2 minutes
+            scheduler.scheduleAtFixedRate(() -> {
+               stocks.RestoreData();
+               stocks.UpdatePrices();
+                System.out.println("hello");
+
+            }, 0, 10, TimeUnit.SECONDS);
+        }
+
+
+        }
+
+
